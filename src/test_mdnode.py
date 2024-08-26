bead58c14d722cebdc9f73d5d6bc1cb6921881e6
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-08-26 05:57:09 -0300

    fix bug on multiword

--- src/test_mdnode.py
+++ src/test_mdnode.py
@@ -1,16 +1,79 @@
-from mdnode import split_nodes_delimiter
-from textnode import *
 import unittest
+from mdnode import *
+from textnode import *
+
 
 class TestMDNode(unittest.TestCase):
-    def test_splitter(self):
-        node = TextNode("This is text with a `code block` word", text_type_text)
-        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+    def test_bold(self):
+        node = TextNode("This is text with a **bolded** word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("bolded", text_type_bold),
+                TextNode(" word", text_type_text),
+            ],
+            new_nodes,
+        )
 
-        self.assertEqual(new_nodes, [TextNode("This is text with a ", text_type_text), TextNode("code block", text_type_code), TextNode(" word", text_type_text),])
+    def test_bold_bold(self):
+        node = TextNode(
+            "This is text with a **bolded** word and **another**", text_type_text
+        )
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("bolded", text_type_bold),
+                TextNode(" word and ", text_type_text),
+                TextNode("another", text_type_bold),
+            ],
+            new_nodes,
+        )
 
-    def test_end_with_delimiter(self):
-        node = TextNode("This is text that ends with a `delimiter`", text_type_text)
-        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+    def test_2bold_3bold(self):
+        node = TextNode(
+            "This text contains this **bolded word** and this **other bolded word**", text_type_text
+        )
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("This text contains this ", text_type_text),
+                TextNode("bolded word", text_type_bold),
+                TextNode(" and this ", text_type_text),
+                TextNode("other bolded word", text_type_bold),
+            ],
+            new_nodes,
+        )
 
-        self.assertEqual(new_nodes, [TextNode("This is text that ends with a ", text_type_text), TextNode("delimiter", text_type_code),])
+    def test_4bold_1bold(self):
+        node = TextNode(
+            "This is text with a **very very bolded word** and **this**", text_type_text
+        )
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        self.assertListEqual(
+            [
+                TextNode("This is text with a ", text_type_text),
+                TextNode("very very bolded word", text_type_bold),
+                TextNode(" and ", text_type_text),
+                TextNode("this", text_type_bold),
+            ],
+            new_nodes,
+        )
+
+    def test__bold_italic(self):
+        node = TextNode("**bold** and *italic*", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
+        new_nodes = split_nodes_delimiter(new_nodes, "*", text_type_italic)
+        self.assertListEqual(
+            [
+                TextNode("bold", text_type_bold),
+                TextNode(" and ", text_type_text),
+                TextNode("italic", text_type_italic),
+            ],
+            new_nodes,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
