1277d09da4f5bd12c6a650a59fbd3f6409621e71
Barrioslopezfd <barrioslopezfd@gmail.com>
2024-09-02 11:55:45 -0300

    now we have img and link extraction

--- src/mdnode.py
+++ src/mdnode.py
@@ -1,4 +1,4 @@
-from re import split
+import re
 from textnode import *
 
 def split_nodes_delimiter(old_nodes, delimeter, text_type):
@@ -36,4 +36,66 @@ def split_nodes_delimiter(old_nodes, delimeter, text_type):
                 new_nodes.append(TextNode(text, text_type_text))
     return new_nodes
 
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
 
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
+
+
+def split_nodes_image(old_nodes: list[TextNode]):
+    node_list = []
+    link_list = []
+    aux = 0
+    for nodes in old_nodes:
+        old_text = nodes.text
+        links = extract_markdown_images(old_text)
+        for l in links:
+            l = list(l)
+            link_list.append(TextNode(l[0], text_type_image, l[1]))
+        for l in links:
+            for t in l:
+                old_text = old_text.replace(f'![{t}]', f'|![text]')
+                old_text = old_text.replace(f'({t})', f'(link)|')
+        old_text = old_text.split("|")
+        if old_text[-1]=='':
+            old_text=old_text[:-1]
+        node_list = list(old_text[:])
+        
+        for i in range(len(node_list)):
+            if "![text](link)" not in node_list[i]:
+                node_list[i] = TextNode(node_list[i], text_type_text)
+            else:
+                node_list[i] = link_list[aux]
+                aux+=1
+
+    return node_list
+
+
+def split_nodes_link(old_nodes: list[TextNode]):
+    node_list = []
+    link_list = []
+    aux = 0
+    for nodes in old_nodes:
+        old_text = nodes.text
+        links = extract_markdown_links(old_text)
+        for l in links:
+            l = list(l)
+            link_list.append(TextNode(l[0], text_type_link, l[1]))
+        for l in links:
+            for t in l:
+                old_text = old_text.replace(f'[{t}]', f'|[text]')
+                old_text = old_text.replace(f'({t})', f'(link)|')
+        old_text = old_text.split("|")
+        if old_text[-1]=='':
+            old_text=old_text[:-1]
+        node_list = list(old_text[:])
+        
+        for i in range(len(node_list)):
+            if "[text](link)" not in node_list[i]:
+                node_list[i] = TextNode(node_list[i], text_type_text)
+            else:
+                node_list[i] = link_list[aux]
+                aux+=1
+
+    return node_list
--- src/test_mdnode.py
+++ src/test_mdnode.py
@@ -61,7 +61,7 @@ class TestMDNode(unittest.TestCase):
             new_nodes,
         )
 
-    def test__bold_italic(self):
+    def test_bold_italic(self):
         node = TextNode("**bold** and *italic*", text_type_text)
         new_nodes = split_nodes_delimiter([node], "**", text_type_bold)
         new_nodes = split_nodes_delimiter(new_nodes, "*", text_type_italic)
@@ -74,6 +74,55 @@ class TestMDNode(unittest.TestCase):
             new_nodes,
         )
 
+    def test_img_img(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+        cosa = extract_markdown_images(text)
+        res = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(cosa, res)
 
+    def test_link_link(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        cosa = extract_markdown_links(text)
+        res = [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(cosa, res)
+
+    def test_img_link1(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        cosa = extract_markdown_images(text)
+        res = [("rick roll", "https://i.imgur.com/aKaOqIh.gif")]
+        self.assertEqual(cosa, res)
+
+    def test_img_link2(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        cosa = extract_markdown_links(text)
+        res = [("to youtube", "https://www.youtube.com/@bootdotdev")]
+        self.assertEqual(cosa, res)
+    
+    def test_link(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(new_nodes, [
+            TextNode("This is text with a link ", text_type_text),
+            TextNode("to boot dev", text_type_link, "https://www.boot.dev"),
+            TextNode(" and ", text_type_text),
+            TextNode("to youtube", text_type_link, "https://www.youtube.com/@bootdotdev"),
+        ])
+        
+        
+    def test_img(self):
+        node = TextNode(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
+            text_type_text,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(new_nodes, [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("rick roll", text_type_image, "https://i.imgur.com/aKaOqIh.gif"),
+            TextNode(" and ", text_type_text),
+            TextNode("obi wan", text_type_image, "https://i.imgur.com/fJRm4Vk.jpeg"),
+        ])
 if __name__ == "__main__":
     unittest.main()
